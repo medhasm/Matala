7cc7779cbaf2dc47930466b0f0ba3d6db9a074c4
Medhat <medhatsmar@hotmail.com>
2019-12-09 06:32:35 +0200

    update method F in CMFU

--- src/Ex1/ComplexFunction.java
+++ src/Ex1/ComplexFunction.java
@@ -19,6 +19,43 @@ this.right=f2;
 	public double f(double x) {
 		// TODO Auto-generated method stub
 		
+		
+		switch (this.P) {
+		case Plus:
+			
+			if (this.left instanceof Polynom || this.left instanceof Polynom_able) {
+				if(this.right instanceof Monom) {
+					
+					Polynom_able PO=(Polynom_able) this.left;
+					Monom m= (Monom) this.right;
+					PO.add(m);
+					
+				}
+	if(this.right instanceof Polynom || this.right instanceof Polynom_able) {
+					
+					Polynom_able PO=(Polynom_able) this.left;
+					Polynom_able m= (Polynom_able) this.right;
+					PO.add(m);
+					
+				}
+			}
+			if(this.left instanceof Monom) {
+				if (this.right instanceof Polynom || this.right instanceof Polynom_able) {
+					Polynom_able PO=(Polynom_able) this.right;
+					Monom m= (Monom) this.left;
+					PO.add(m);
+				}
+				if(this.right instanceof Monom) {
+					Monom m=(Monom) this.left;
+					Monom n=(Monom) this.right;
+					m.add(n);
+				}
+			}
+			break;
+
+		default:
+			break;
+		}
 		return 0;
 	}
 
@@ -31,7 +68,8 @@ this.right=f2;
 	@Override
 	public function copy() {
 		// TODO Auto-generated method stub
-		return null;
+		ComplexFunction cf=new ComplexFunction(this.P,this.left,this.right);
+		return cf;
 	}
 
 	@Override
@@ -75,31 +113,55 @@ this.right=f2;
 	@Override
 	public void max(function f1) {
 		// TODO Auto-generated method stub
+		if (f1==null) {
+			return ;
+		}
+		this.left=new ComplexFunction(this.P,this.left,this.right);
+		this.right=f1;
+		this.P=P.Max;
 		
 	}
+	
+	
 
 	@Override
 	public void min(function f1) {
 		// TODO Auto-generated method stub
-		
+		if (f1==null) {
+			return ;
+		}
+		this.left=new ComplexFunction(this.P,this.left,this.right);
+		this.right=f1;
+		this.P=P.Min;
+	
 	}
 
 	@Override
 	public void comp(function f1) {
 		// TODO Auto-generated method stub
+		if (f1==null) {
+			return ;
+		}
+		this.left=new ComplexFunction(this.P,this.left,this.right);
+		this.right=f1;
+		this.P=P.Comp;
+	
 		
 	}
 
 	@Override
 	public function left() {
 		// TODO Auto-generated method stub
-		return null;
+		if(this.left==null) {
+			throw new RuntimeException("ERR the left function can't be nuull : "+this.left);
+		}
+		return this.left;
 	}
 
 	@Override
 	public function right() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.right;
 	}
 
 	@Override
