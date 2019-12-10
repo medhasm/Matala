f52cf836568e06afb401bacde412f0241e6be4b5
AseelNemer <AseelNemer@users.noreply.github.com>
2019-12-10 20:35:33 +0200

    update function GUI

--- src/Ex1/Functions_GUI.java
+++ src/Ex1/Functions_GUI.java
@@ -1,14 +1,27 @@
 package Ex1;
+import java.awt.Color;
+import java.awt.Font;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
 
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Iterator;
 
 public class Functions_GUI implements functions {
-
+	
+	ArrayList<function> func=new ArrayList<function>();
 	@Override
 	public boolean add(function arg0) {
 		// TODO Auto-generated method stub
+		func.add(arg0);
 		return false;
 	}
 
@@ -27,30 +40,38 @@ public class Functions_GUI implements functions {
 	@Override
 	public boolean contains(Object arg0) {
 		// TODO Auto-generated method stub
+		if (arg0 instanceof function )
+		{
+			return func.contains(arg0);
+		}
 		return false;
 	}
 
 	@Override
 	public boolean containsAll(Collection<?> arg0) {
 		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
-		return false;
+		return func.isEmpty();
 	}
 
 	@Override
 	public Iterator<function> iterator() {
 		// TODO Auto-generated method stub
-		return null;
+		
+		return this.func.iterator();
 	}
 
 	@Override
 	public boolean remove(Object arg0) {
 		// TODO Auto-generated method stub
+		if(arg0 instanceof function )
+			 return func.remove(arg0);
 		return false;
 	}
 
@@ -107,5 +128,12 @@ public class Functions_GUI implements functions {
 		// TODO Auto-generated method stub
 		
 	}
+
+	public ComplexFunction get(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
 
 }
