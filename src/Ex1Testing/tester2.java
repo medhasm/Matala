27ae95a8072a8abc68f040d21ade083780b68ebf
Medhat <medhatsmar@hotmail.com>
2019-12-12 21:35:50 +0200

    update

---
+++ src/Ex1Testing/tester2.java
@@ -0,0 +1,69 @@
+package Ex1Testing;
+
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import Ex1.ComplexFunction;
+import Ex1.Functions_GUI;
+import Ex1.Monom;
+import Ex1.Operation;
+import Ex1.Polynom;
+import Ex1.Range;
+import Ex1.function;
+import Ex1.functions;
+
+import java.util.Iterator;
+
+public class tester2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		functions data = FunctionsFactory();
+		int w=1000, h=600, res=200;
+		Range rx = new Range(-10,10);
+		Range ry = new Range(-5,15);
+		data.drawFunctions(w,h,rx,ry,res);
+	}
+	public static functions FunctionsFactory() {
+		functions ans = new Functions_GUI();
+		String s1 = "3.1 +2.4x^2 -x^4";
+		String s2 = "5 +2x -3.3x +0.1x^5";
+		String[] s3 = {"x +3","x -2", "x -4"};
+		Polynom p1 = new Polynom(s1);
+		Polynom p2 = new Polynom(s2);
+		Polynom p3 = new Polynom(s3[0]);
+		ComplexFunction cf3 = new ComplexFunction(p3);
+		for(int i=1;i<s3.length;i++) {
+			cf3.mul(new Polynom(s3[i]));
+		}
+		
+		ComplexFunction cf = new ComplexFunction(Operation.Plus, p1,p2);
+		ComplexFunction cf4 = new ComplexFunction("Divid", new Polynom("x +1"),cf3);
+		cf4.plus(new Monom("2"));
+		ans.add(cf.copy());
+		ans.add(cf4.copy());
+		cf.div(p1);
+		ans.add(cf.copy());
+		String s = cf.toString();
+		function cf5 = cf4.initFromString(s1);
+		function cf6 = cf4.initFromString(s2);
+		ans.add(cf5.copy());
+		ans.add(cf6.copy());
+		Iterator<function> iter = ans.iterator();
+		function f = iter.next();
+		ComplexFunction max = new ComplexFunction(f);
+		ComplexFunction min = new ComplexFunction(f);
+		while(iter.hasNext()) {
+			f = iter.next();
+			max.max(f);
+			min.min(f);
+		}
+		ans.add(max);
+		ans.add(min);		
+		return ans;
+	}
+}
