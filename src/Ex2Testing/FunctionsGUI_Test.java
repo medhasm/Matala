8e140a64ca9fddecdfa82f14106dabfd07d6de7c
Medhat <medhatsmar@hotmail.com>
2019-12-12 23:47:53 +0200

    update junits for fgui

--- src/Ex2Testing/FunctionsGUI_Test.java
+++ src/Ex2Testing/FunctionsGUI_Test.java
@@ -6,6 +6,12 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import Ex1Testing.ComplexFunction;
+import Ex1Testing.Functions_GUI;
+import Ex1Testing.IOException;
+import Ex1Testing.Monom;
+import Ex1Testing.Polynom;
+
 public class FunctionsGUI_Test {
 
 	@Before
@@ -83,22 +89,63 @@ public class FunctionsGUI_Test {
 
 	@Test
 	public void testInitFromFile() {
-		fail("Not yet implemented"); // TODO
+		Functions_GUI ans =new Functions_GUI();
+		ans.add(new Polynom("3x^5+7"));
+		ans.add(new ComplexFunction("Plus",new Polynom ("4x^5+3"),new Monom("24x")));
+		ans.add(new Polynom("x^5+x^4+x^3+x^2+x"));
+
+		try {
+			ans.saveToFile("function.txt");
+		_data.initFromFile("function.txt");	
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		assertEquals(ans.toString(),_data.toString());
+
 	}
+	
 
 	@Test
 	public void testSaveToFile() {
-		fail("Not yet implemented"); // TODO
+		Functions_GUI ans = new Functions_GUI();
+		Functions_GUI _data = new Functions_GUI();
+		ans.add(new Polynom("-4x^5+ 5x^6+5"));
+		ans.add(new ComplexFunction("Divid(Times(x^2,x),4)"));
+		ans.add(new Monom("3x^7"));
+		try {
+			ans.saveToFile("function.txt");
+			_data.initFromFile("function.txt");
+		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+		assertEquals(_data.toString(), ans.toString());
+
+	}
 	}
 
 	@Test
 	public void testDrawFunctionsIntIntRangeRangeInt() {
-		fail("Not yet implemented"); // TODO
+		Functions_GUI ans = new Functions_GUI();
+ans.add(new Polynom("3x^2"));
+
+		int w=1000, h=600, res=200;
+		Range rx = new Range(-10,10);
+		Range ry = new Range(-5,15);
+		ans.drawFunctions(w,h,rx,ry,res);
 	}
 
 	@Test
 	public void testDrawFunctionsString() {
-		fail("Not yet implemented"); // TODO
+		Functions_GUI ans = new Functions_GUI();
+ans.add(new Polynom("4x^2+3"));
+
+ans.drawFunctions("GUI_params.txt");
 	}
 
 	@Test
--- src/Ex2Testing/Monomtest.java
+++ src/Ex2Testing/Monomtest.java
@@ -6,15 +6,22 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import Ex1Testing.BeforeEach;
+import Ex1Testing.Exception;
+import Ex1Testing.Monom;
+import Ex1Testing.RuntimeException;
+
 public class Monomtest {
-
-	@Before
-	public void setUp() throws Exception {
+	Monom m ,d ,e;
+	@BeforeEach
+	void init()
+	{
+		  m=new Monom();
+		  d=new Monom();
+		  e= new Monom();
 	}
 
-	@After
-	public void tearDown() throws Exception {
-	}
+
 
 	@Test
 	public void testGetComp() {
@@ -48,17 +55,37 @@ public class Monomtest {
 
 	@Test
 	public void testDerivative() {
-		fail("Not yet implemented"); // TODO
+		 System.out.println("derivative Test:");
+   	  m=new Monom(3,2);
+   	  d =new Monom(m.derivative());
+   	  e=new Monom(6,1);
+   	 
+   	 if(d.get_coefficient()!=e.get_coefficient() || d.get_power()!=e.get_power()) {
+   		 System.out.println("derivative fail");
+   		 
+   	 }
 	}
 
 	@Test
 	public void testF() {
-		fail("Not yet implemented"); // TODO
+   	 System.out.println("function test:");
+   	 m=new Monom(4,2);
+   	 double f;
+   	 f=m.f(3);
+
+   	 assertEquals(f,36,"f method fail");
+   	 
+    }
 	}
 
 	@Test
 	public void testIsZero() {
-		fail("Not yet implemented"); // TODO
+   	 System.out.println("isZeroTest:");
+   	 m=new Monom(0,4);
+   	 if(!m.isZero()) {
+   		 System.out.println("isZero Failed");
+   		 
+   	 }
 	}
 
 	@Test
@@ -68,42 +95,92 @@ public class Monomtest {
 
 	@Test
 	public void testAdd() {
-		fail("Not yet implemented"); // TODO
+   	 System.out.println("test add:");
+   	 m=new Monom(3,2);
+   	 d=new Monom(5,2);
+   	 m.add(d);
+   	 Monom X=new Monom(8,2);
+   	 assertNotSame(X.toString() ,m.toString(),"add method of monom failed");
 	}
-
+    @Test
+    void addException(){
+   	 System.out.println("test addexception:");
+   	 System.out.println("add Exception:");
+   	 try {
+   		 m=new Monom(3,2);
+       	 d=new Monom(5,3);
+   		 m.add(d);
+   		 fail("add didnt throw exception ");
+   	 }catch (Exception e) { }
+    } 
 	@Test
 	public void testMultipy() {
-		fail("Not yet implemented"); // TODO
+	 
+	    	 System.out.println("test multipy:");
+	    
+	    	 m=new Monom(3,2);
+	    	 d=new Monom(2,3);
+	    	 m.multipy(d);
+	    	 e=new Monom(6,5);
+	    	 assertNotSame(e.toString() ,m.toString(),"multiply method of monom failed");
+	    	
 	}
 
 	@Test
 	public void testToString() {
-		fail("Not yet implemented"); // TODO
+   	 System.out.println("test tostring:");
+   	 m=new Monom(3,2);
+   	 assertNotSame("3.0x^2",m.toString(),"Strings not equals");
 	}
 
 	@Test
 	public void testSet_coefficient1() {
-		fail("Not yet implemented"); // TODO
+   	 System.out.println("test setcoefficient:");
+   	 m.set_coefficient1(3);
+   	 assertEquals((double)3,m.get_coefficient(),"set coefficient failed");
 	}
 
 	@Test
 	public void testSet_power1() {
-		fail("Not yet implemented"); // TODO
+   	 System.out.println("test setpower n:");
+   	 m.set_power1(3);
+   	 assertEquals(3,m.get_power(),"set coefficient failed");
 	}
-
+    @Test
+    void setPowerException() {
+   	 System.out.println("test setpower exception:");
+   	 try {
+   	 m.set_power1(-1);
+   	 fail("didnt throw exception to negatiove power");
+   	 }catch(RuntimeException e) {
+    }
+    }
 	@Test
 	public void testInitFromString() {
-		fail("Not yet implemented"); // TODO
+   	 System.out.println("test initFromString:");
+   	 m= (Monom) m.initFromString("3x^2");
+   	 d=new Monom(3,2);
+   	 assertNotSame(d.toString(),m.toString(),"initfromstring failed");
 	}
 
 	@Test
 	public void testCopy() {
-		fail("Not yet implemented"); // TODO
+   	 System.out.println("test copy");
+   	 m=new Monom(3,2);
+   	 d=(Monom) m.copy();
+   	 e=new Monom(3,2);
+   	
+   	 assertNotSame(e.toString(),d.toString(),"copy method failed");
 	}
 
 	@Test
 	public void testEqualsObject() {
-		fail("Not yet implemented"); // TODO
+   	 System.out.println("equaltest:");
+   	 m=new Monom(3,2);
+   	 e=new Monom(3,2);
+   	 if(!m.equals(e)) {
+   		 System.out.println("equals method failed");
+   	 }
 	}
 
 	@Test
